f1ee2dc13f3babc1e970b17928e6c0a36f56183a
779440 <7794470@gmail.com>
2023-05-06 16:48:09 +0500

    add file

--- Test3.py
+++ Test3.py
@@ -1,9 +1,26 @@
 from aiogram import Bot, Dispatcher, types, executor
 from config import TOKEN_API
 
+HELP_COMMAND = """
+/help - список команд
+/start - начать работу с ботом
+"""
+
 
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
 
 
+@dp.message_handler(commands=['help'])
+async def help_command(message: types.Message):
+    await message.reply(text=HELP_COMMAND)
 
+
+@dp.message_handler(commands=['start'])
+async def help_command(message: types.Message):
+    await message.answer(text="Welcome to Telegram Bot")
+    await message.delete()
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
